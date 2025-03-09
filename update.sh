c1c07b68fcdb27479ea6a1613d828a1291ec3356
Jeremie Queenton <jeremie.queenton.1@ulaval.ca>
2025-03-08 19:20:05 -0500

    final backend config (i think)

--- update.sh
+++ update.sh
@@ -10,11 +10,14 @@ docker rm blackbox-container
 echo "Rebuilding Docker image..."
 docker build -t blackbox .
 
+echo "Ensuring Docker volume exists..."
+docker volume create blackbox_db
+
 echo "Copying the latest database into Docker volume..."
-docker run --rm -v blackbox_db:/data -v "/k/My Drive/DB.sqlite:/tmp/DB.sqlite" alpine sh -c "cp -f /tmp/DB.sqlite /data/Base_de_Donnees.sqlite"
+docker run --rm -v blackbox_db:/data -v "/k/My Drive/DB.sqlite:/tmp/DB.sqlite" alpine sh -c "cp -f /tmp/DB.sqlite /data/DB.sqlite"
 
 echo "Starting new container with updated database..."
 docker run -d --restart unless-stopped -p 3000:3000 \
-    --name blackbox-container -v blackbox_db:/app/Base_de_Donnees.sqlite blackbox
+    --name blackbox-container -v blackbox_db:/app/DB.sqlite blackbox
 
 echo "Update completed successfully!"
