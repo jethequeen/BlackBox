51e3bdee5376e4286e269c4b9acd1bbbe2fed3c5
Jeremie Queenton <jeremie.queenton.1@ulaval.ca>
2025-03-09 19:17:29 -0400

    database persistence

--- update.sh
+++ update.sh
@@ -19,6 +19,6 @@ docker image prune -f
 
 # Start the new container with the database inside
 echo "Starting new container with database..."
-docker run -d -p 3000:3000 --name blackbox-container -e IN_DOCKER=true -v "//k/My Drive/DB.sqlite:/app/Base_de_Donnees.sqlite" blackbox
+docker run -d -p 3000:3000 --name blackbox-container -e IN_DOCKER=true -v "//c/BlackBox DB/DB.sqlite:/app/Base_de_Donnees.sqlite" blackbox
 
 echo "Update completed successfully!"
