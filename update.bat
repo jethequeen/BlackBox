2bc7824538b09ec5a6aeb50a7aed455b217112ea
Jeremie Queenton <jeremie.queenton.1@ulaval.ca>
2025-03-10 11:07:46 -0400

    Fix somes useless things

--- update.bat
+++ update.bat
@@ -1,6 +1,12 @@
 @echo off
 cd C:\BlackBox
 git pull origin master
-pm2 kill
+
+:: Check if the process exists before deleting
+pm2 describe BlackBox >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    pm2 delete BlackBox
+)
+
 pm2 start server.js --name BlackBox
 echo Update completed!
